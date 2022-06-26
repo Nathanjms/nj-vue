96427529245ed1a0e6ef5ab156a021aa119a3c78
Nathan James <nathanjms@outlook.com>
2022-06-26 20:59:26 +0100

    WIP Navbar

---
+++ src/components/NavItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps(['name', 'link', 'icon']);
+</script>
+<template>
+    <div class="flex-1 group hover:scale-110">
+        <a :href="link" class="flex items-end justify-center text-center 
+                mx-auto px-2 pt-2 w-full 
+                text-gray-400 
+                group-hover:text-purple-700 
+                border-b-2 border-transparent
+        ">
+            <span class="block px-1 pt-1 pb-2">
+                <i :class="icon" class="text-2xl pt-1 mb-1 block`"></i>
+                <span class="block text-xs pb-1">{{ name }}</span>
+                <span class="block w-1 mx-auto h-1 group-hover:bg-purple-500 rounded-full"></span>
+            </span>
+        </a>
+    </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+    <style>
+    @import url(https://pro.fontawesome.com/releases/v5.10.0/css/all.css);
+    </style>
+<script setup>
+import NavItem from './NavItem.vue';
+const navItems = [
+    {
+        link: '#',
+        name: 'Home',
+        icon: 'far fa-home'
+    },
+    {
+        link: '#',
+        name: 'About',
+        icon: 'far fa-user'
+    },
+    {
+        link: '#',
+        name: 'Coding',
+        icon: 'far fa-desktop'
+    },
+    {
+        link: '#',
+        name: 'Music',
+        icon: 'far fa-guitar'
+    },
+];
+</script>
+<template>
+
+    <div class="mx-auto fixed z-50 bottom-0 md:top-0 w-full md:w-auto md:flex md:items-center">
+        <div class="px-1 bg-white shadow-lg rounded-2xl">
+            <div class="flex md:flex-col md:py-1 dark:bg-slate-100">
+                <NavItem v-for="navItem in navItems" :link="navItem.link" :name="navItem.name" :icon="navItem.icon" />
+            </div>
+        </div>
+    </div>
+</template>
