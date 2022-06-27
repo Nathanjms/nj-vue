cec3cc986698ee2d62d658d5fb832f888542c5b0
Nathan James <nathanjms@outlook.com>
2022-06-27 18:54:59 +0100

    Home Component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,15 @@
 import './assets/css/index.css';
 import Navbar from './components/Navbar.vue';
 import DarkModeToggle from './components/DarkModeToggle.vue';
+import Home from './components/Home.vue';
 </script>
 
 <template>
   <div class="h-screen dark:bg-slate-700 bg-slate-200">
-
     <Navbar />
     <div class="container mx-auto">
       <DarkModeToggle />
-      <div class="bg-slate-400 py-5 mx-auto w-11/12 sm:w-4/6 text-center my-5 rounded-lg shadow-lg">
-        <h1 class="text-3xl font-medium pb-4">
-          Nathan James
-        </h1>
-        <h2 class="text-2xl font-medium py-4 hover:rotate-12 transition-all">
-          Website coming soon...
-        </h2>
-      </div>
+      <Home />
     </div>
   </div>
 </template>
