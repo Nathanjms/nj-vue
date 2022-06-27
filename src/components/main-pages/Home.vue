9f309903715180fdd196f3f6b617d440af64d95d
Nathan James <nathanjms@outlook.com>
2022-06-27 20:11:10 +0100

    Add link to old website

--- src/components/main-pages/Home.vue
+++ src/components/main-pages/Home.vue
@@ -6,5 +6,7 @@
         <h2 class="text-2xl font-medium py-4 hover:rotate-12 transition-all">
             Website coming soon...
         </h2>
+        <a href="https://old.nathanjms.co.uk" class="text-sm text-blue-600 hover:text-blue-800 underline">Go to Old
+            Website?</a>
     </div>
 </template>
