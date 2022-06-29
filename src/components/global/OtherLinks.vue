c278177955c6387eff52f787d745390c9882573e
Nathan James <nathanjms@outlook.com>
2022-06-29 23:17:38 +0100

    OtherLinks

---
+++ src/components/global/OtherLinks.vue
@@ -0,0 +1,23 @@
+<script setup>
+import OtherLinksItem from './OtherLinksItem.vue';
+const otherLinks = [
+    {
+        link: 'https://github.com/nathanjms',
+        icon: 'fab fa-github',
+        newTab: true
+    },
+    {
+        link: 'https://www.linkedin.com/in/nathan-jms/',
+        icon: 'fab fa-linkedin',
+        newTab: true
+    },
+    {
+        link: 'mailto:nathan@nathanjms.co.uk',
+        icon: 'far fa-envelope',
+        newTab: false
+    },
+];
+</script>
+<template>
+    <OtherLinksItem v-for="otherLink in otherLinks" :link="otherLink.link" :icon="otherLink.icon" :newTab="otherLink.newTab"/>
+</template>
---
+++ src/components/global/OtherLinksItem.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['link', 'icon', 'newTab']);
+</script>
+<template>
+    <a :href="link" :target="newTab ? '_blank' : null" :rel="newTab ? 'noopener noreferrer' : null">
+        <span class="p-2 mx-2 rounded-xl dark:bg-slate-200 bg-slate-700
+                text-white dark:text-gray-700
+                w-2">
+            <i :class="icon"></i>
+        </span>
+    </a>
+</template>
