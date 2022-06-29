93c7188434f35d9d9c03ec60f60860b10188346b
Nathan James <nathanjms@outlook.com>
2022-06-29 22:24:22 +0100

    Add email

--- src/components/main-pages/Home.vue
+++ src/components/main-pages/Home.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="bg-slate-400 py-5 mx-auto w-11/12 sm:w-4/6 text-center my-5 rounded-lg shadow-lg">
-        <h1 class="text-3xl font-medium pb-4">
+        <h1 class="text-3xl font-medium">
             Nathan James
         </h1>
+        <h2>nathan@nathanjms.co.uk</h2>
         <h2 class="text-2xl font-medium py-4 hover:rotate-12 transition-all">
             Website coming soon...
         </h2>
-        <a href="https://old.nathanjms.co.uk" class="text-sm text-purple-700 hover:text-purple-800 underline">Go to Old
+        <a href="https://old.nathanjms.co.uk" class="text-sm text-blue-600 hover:text-blue-800 underline">Go to Old
             Website?</a>
     </div>
 </template>
