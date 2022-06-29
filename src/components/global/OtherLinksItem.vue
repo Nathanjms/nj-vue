fe827b4c1462a609ec063d79c4d41e1a00cdd5fa
Nathan James <nathanjms@outlook.com>
2022-06-29 23:30:07 +0100

    Nice transitions

--- src/components/global/OtherLinksItem.vue
+++ src/components/global/OtherLinksItem.vue
@@ -3,10 +3,11 @@ defineProps(['link', 'icon', 'newTab']);
 </script>
 <template>
     <a :href="link" :target="newTab ? '_blank' : null" :rel="newTab ? 'noopener noreferrer' : null">
-        <span class="p-2 mx-2 rounded-xl dark:bg-slate-200 bg-slate-700
+        <button class="p-2 mx-2 rounded-xl dark:bg-slate-200 bg-slate-700
                 text-white dark:text-gray-700
-                w-2">
+                w-10 hover:text-purple-400 dark:hover:text-purple-700
+                transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300">
             <i :class="icon"></i>
-        </span>
+        </button>
     </a>
 </template>
