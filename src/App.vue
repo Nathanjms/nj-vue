a6abf22cd3f3591adcf1c8995480e3b7c1fdfe38
Nathan James <nathanjms@outlook.com>
2022-06-29 23:47:45 +0100

    Transition when changing between dark/light mode

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import OtherLinks from './components/global/OtherLinks.vue';
 </script>
 
 <template>
-  <div class="h-screen dark:bg-slate-700 bg-slate-200">
+  <div class="h-screen transition ease-in-out dark:bg-slate-700 bg-slate-200">
     <Navbar />
     <div class="container mx-auto">
       <div class="p-5 flex justify-between text-gray-700 dark:text-white">
