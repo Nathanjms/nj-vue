b04bf2ee54a8c308fa1ab35f05b8e8a7f6574117
Nathan James <nathanjms@outlook.com>
2022-06-30 10:34:09 +0100

    Minor update to classes

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import OtherLinks from './components/global/OtherLinks.vue';
 </script>
 
 <template>
-  <div class="min-h-[calc(100vh)] sm:min-h-screen transition ease-in-out dark:bg-slate-700 bg-slate-200">
+  <div class="min-h-screen sm:min-h-screen transition ease-in-out dark:bg-slate-700 bg-slate-200">
     <Navbar />
     <div class="container mx-auto pb-16">
       <div class="p-5 flex justify-between text-gray-700 dark:text-white">
@@ -17,7 +17,8 @@ import OtherLinks from './components/global/OtherLinks.vue';
           <DarkModeToggle />
         </div>
       </div>
-      <div class="bg-slate-400 py-5 px-3 mx-auto w-11/12 sm:w-5/6 min-h-[50vh] text-center my-5 rounded-lg shadow-lg">
+      <div
+        class="bg-slate-400 py-5 px-3 mx-auto w-11/12 sm:w-5/6 min-h-[50vh] sm:min-h-[70vh] text-center my-5 rounded-lg shadow-lg">
         <router-view />
       </div>
     </div>
