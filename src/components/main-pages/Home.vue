ceebc91a315b6e0bd5cae959b2f7d0fa3f825a4b
Nathan James <nathanjms@outlook.com>
2022-09-03 16:00:35 +0100

    Include in Home.vue and tidy up

--- src/components/main-pages/Home.vue
+++ src/components/main-pages/Home.vue
@@ -1,11 +1,20 @@
+<script setup>
+import { AutoTyperVue } from "auto-typer-vue3";
+</script>
+
 <template>
-    <h1 class="text-3xl font-medium">
-        Nathan James
-    </h1>
-    <h2 class="break-all">nathan@nathanjms.co.uk</h2>
-    <h2 class="py-4 text-2xl font-medium transition-all hover:rotate-3">
-        Website coming soon...
-    </h2>
-    <a href="https://old.nathanjms.co.uk" class="text-sm text-blue-600 underline hover:text-blue-800">Go to Old
-        Website?</a>
+  <h1 class="text-3xl font-medium">Nathan James</h1>
+  <h2 class="break-all">nathan@nathanjms.co.uk</h2>
+  <a
+    href="https://old.nathanjms.co.uk"
+    class="text-sm text-blue-600 underline hover:text-blue-800"
+    >Go to Old Website?</a
+  >
+
+  <AutoTyperVue
+    beginningWord="&nbsp;"
+    componentTag="h2"
+    class="py-4 text-2xl font-medium transition-all hover:rotate-3"
+    text="Website coming soon..."
+  />
 </template>
