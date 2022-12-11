a1788003a83e4370977e581f7f7d28829d33cba4
Nathan James <nathanjms@outlook.com>
2022-12-11 22:36:27 +0000

    Music Page

--- src/components/pages/MusicPage.vue
+++ src/components/pages/MusicPage.vue
@@ -1,10 +1,35 @@
+<script setup>
+import LinkPartial from "../global/LinkPartial.vue";
+</script>
+
 <template>
-  <div class="grid grid-rows-3">
-    <div class="mb-4">
+  <div>
+    <div class="mb-8">
       <h1 class="text-3xl font-medium">Music</h1>
     </div>
-    <div>
-      <p class="mb-4">Coming Soon...</p>
+    <div class="text-left">
+      <p class="mb-4">
+        As well as enjoying playing covers, I've got a catalogue of my own songs which are mostly unrecorded and
+        unpolished.
+      </p>
+      <p class="mb-4">
+        There are a couple that I have been able to record a little more professionally, and you can listen to one of
+        these below.
+      </p>
+      <p>
+        You'll find another one of my songs as the backing to one of my
+        <LinkPartial href="https://youtube.com/nathanjms"> YouTube Videos</LinkPartial>, but these are the only ones in
+        the wild, for now...
+      </p>
+      <iframe
+        class="w-full mx-auto rounded mt-8"
+        height="108"
+        scrolling="no"
+        title="Happy Right Now"
+        frameBorder="no"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/996209464&color=%237b26ad&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+      >
+      </iframe>
     </div>
   </div>
 </template>
