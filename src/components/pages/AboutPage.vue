0dd748b9800e59212b01a2feb39781da509ac358
Nathan James <nathanjms@outlook.com>
2022-12-11 21:33:41 +0000

    About Page

--- src/components/pages/AboutPage.vue
+++ src/components/pages/AboutPage.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="grid grid-rows-3">
-    <div class="mb-4">
+  <div>
+    <div class="mb-8">
       <h1 class="text-3xl font-medium">About</h1>
     </div>
-    <div>
-      <p class="mb-4">Coming Soon...</p>
+    <div class="text-left">
+      <p class="mb-4">🎓 University of Nottingham Maths Graduate</p>
+      <p class="mb-4">🖥️ Web Developer for just over 2 years</p>
+      <p class="mb-4">
+        ⌨️ Main languages include
+        <a
+          class="font-medium text-blue-700 underline hover:text-blue-900"
+          href="https://laravel.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Laravel</a
+        >
+        and
+        <a
+          class="font-medium text-blue-700 underline hover:text-blue-900"
+          href="https://vuejs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Vue.js</a
+        >
+      </p>
+      <p class="mb-4">🎸 Love going to gigs and playing the guitar</p>
     </div>
   </div>
 </template>
